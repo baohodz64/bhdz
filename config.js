dea230cf596a5be0e3f9c57072bb49d250e84e5c
baohodz64 <143327058+baohodz64@users.noreply.github.com>
2023-08-27 18:38:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+//æ³¨å†Œé“¾æ¥
+const config_registerLinks = [
+    "https://www.555win55.com/?a=100",
+    "https://www.555win55.com/?a=100",
+    "https://www.555win55.com/?a=100",
+    "https://www.555win55.com/?a=100",
+    "https://www.555win55.com/?a=100",
+    "https://www.555win55.com/?a=100"
+]
+const config_phone1 ='131242202100'//æ‰‹æœºå·ç 1
+const config_phone2 = '17645485273'//æ‰‹æœºå·ç 2
+//å®¢æœåœ°å€
+const config_kefu_url = "https://www.win55kf.com"
+//ç«™ç‚¹è·³è½¬åœ°å€
+const config_site_url = "https://www.555win55.com/?a=100"
+const api = "https://137722.cc"//æœå¡å™¨APIåœ°å€
