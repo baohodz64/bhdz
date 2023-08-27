6bd3d2e089ef69b63c380680495bb22570546153
baohodz64 <143327058+baohodz64@users.noreply.github.com>
2023-08-27 19:08:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,4 @@ const config_phone2 = '17645485273'//æ‰‹æœºå·ç 2
 const config_kefu_url = "https://www.win55kf.com"
 //ç«™ç‚¹è·³è½¬åœ°å€
 const config_site_url = "https://www.555win55.com/?a=101"
-const api = "https://www.137722.cc"//æœå¡å™¨APIåœ°å€
+const api = "https://137722.cc"//æœå¡å™¨APIåœ°å€
